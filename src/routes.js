9f5aeb3f06d448d2dac0b2e99e6b206623c00030
Chris Troutner <chris.troutner@gmail.com>
2017-09-23 08:28:17 -0700

    Adding /appdashboard route

--- src/routes.js
+++ src/routes.js
@@ -60,6 +60,48 @@ const routes = [
       }
     ]
   }, {
+    path: '/appdashboard',
+    component: DashView,
+    children: [
+      {
+        path: 'dashboard',
+        alias: '',
+        component: DashboardView,
+        name: 'Dashboard',
+        meta: {description: 'Overview of environment'}
+      }, {
+        path: 'tables',
+        component: TablesView,
+        name: 'Tables',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'tasks',
+        component: TasksView,
+        name: 'Tasks',
+        meta: {description: 'Tasks page in the form of a timeline'}
+      }, {
+        path: 'setting',
+        component: SettingView,
+        name: 'Settings',
+        meta: {description: 'User settings page'}
+      }, {
+        path: 'access',
+        component: AccessView,
+        name: 'Access',
+        meta: {description: 'Example of using maps'}
+      }, {
+        path: 'server',
+        component: ServerView,
+        name: 'Servers',
+        meta: {description: 'List of our servers'}
+      }, {
+        path: 'repos',
+        component: ReposView,
+        name: 'Repository',
+        meta: {description: 'List of popular javascript repos'}
+      }
+    ]
+  }, {
     // not found handler
     path: '*',
     component: NotFoundView
